194425ad71b87730a88bbfc64c1ffe4312c7d8b0
Andjela <trajkovicka.9909@gmail.com>
2022-11-20 10:39:44 +0100

    pass

--- Vezbe05/spring-security-example/src/main/java/rs/ac/uns/ftn/springsecurityexample/config/WebSecurityConfig.java
+++ Vezbe05/spring-security-example/src/main/java/rs/ac/uns/ftn/springsecurityexample/config/WebSecurityConfig.java
@@ -43,23 +43,15 @@ public class WebSecurityConfig {
         return new BCryptPasswordEncoder();
     }
   	
-    // Definisemo nacin utvrdjivanja korisnika pri autentifikaciji
-// 	@Bean
-// 	public DaoAuthenticationProvider authenticationProvider() {
-// 	    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-// 	    // 1. koji servis da koristi da izvuce podatke o korisniku koji zeli da se autentifikuje
-// 	    // prilikom autentifikacije, AuthenticationManager ce sam pozivati loadUserByUsername() metodu ovog servisa
-// 	    authProvider.setUserDetailsService(userDetailsService());
-// 	    // 2. kroz koji enkoder da provuce lozinku koju je dobio od klijenta u zahtevu 
-//	    // da bi adekvatan hash koji dobije kao rezultat hash algoritma uporedio sa onim koji se nalazi u bazi (posto se u bazi ne cuva plain lozinka)
-// 	    authProvider.setPasswordEncoder(passwordEncoder());
-// 	 
-// 	    return authProvider;
-// 	}
+  
  	@Bean
  	public DaoAuthenticationProvider authenticationProvider() {
  	    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+ 	   // 1. koji servis da koristi da izvuce podatke o korisniku koji zeli da se autentifikuje
+ 	    // prilikom autentifikacije, AuthenticationManager ce sam pozivati loadUserByUsername() metodu ovog servisa
  	    authProvider.setUserDetailsService(userDetailsService());
+ 	    // 2. kroz koji enkoder da provuce lozinku koju je dobio od klijenta u zahtevu 
+	    // da bi adekvatan hash koji dobije kao rezultat hash algoritma uporedio sa onim koji se nalazi u bazi (posto se u bazi ne cuva plain lozinka)
  	    authProvider.setPasswordEncoder(passwordEncoder());
  	 
  	    return authProvider;
